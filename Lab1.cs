5522e980d30a9fb9a3dbf31087d07266ec2a5d86
TheBadesman <Cameron@ottleys.net>
2022-09-07 20:06:14 -0600

    Added so much
    just wow

--- Lab1.cs
+++ Lab1.cs
@@ -8,14 +8,27 @@ public class Program
 	public void Main()
 	{
 
+		public int autoCannon.description = true;
+
+		if (autoCannon.description = true)
+		{
+			
+			Console.WriteLine(autoCannon.description);
+
+		}
+
 		autoCannon = new Weapon();
 
 		autoCannon.ammoCount++;
 
 		Console.WriteLine(autoCannon.weaponName + " has a ammoCount of " + autoCannon.ammoCount);
 
+		Console.WriteLine(autoCannon.description);
+
 	}
 }
+
+
 
 public class Weapon
 {
@@ -23,7 +36,19 @@ public class Weapon
 	public int ammoCount = 10;
 	public string weaponName = "Auto Cannon";
 	public float firePower = 15f;
+	public string description = "A heavy support weapon normally used by a team of guardsmen, the Auto Cannon useds heavy shells instead of energy cells to punch through both infantry and light to medium-light vehicles";
 
 
 }
 
+//The class system and the variables assigned to each
+
+public class Guardsmen
+{
+
+	public string loadoutHW = "Heavy Weapons";
+	public int HealthHW = 5;
+	public int AccuracyHW = 60;
+	public float HWPerfered = 1.5f;
+
+}
