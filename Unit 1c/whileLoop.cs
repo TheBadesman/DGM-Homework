d4ec7bde21204af19f8afe494d126f558520cc38
TheBadesman <Cameron@ottleys.net>
2022-09-22 01:08:26 -0600

    Update whileLoop.cs
    Did the while loop

--- Unit 1c/whileLoop.cs
+++ Unit 1c/whileLoop.cs
@@ -6,8 +6,25 @@ public class Program
     public void Main()
     {
 
-    
-    
+    int ammo = 10;
+    while(ammo > 0)
+    {
+
+        Console.WriteLine("You still have " + ammo + " left.");
+        ammo--;
+
     }
+    
+    int squad3 = 5;
+    do
+    {
+        
+        Console.WriteLine("Squad 3 has " + squad3 + " remaining");
+        squad3--;
+
+    }
+    while(squad3 != 0);
+	
+	}
 
 }
