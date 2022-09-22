3401413fd608403d84d430389b3d119d3eadbe86
TheBadesman <Cameron@ottleys.net>
2022-09-21 23:42:51 -0600

    Update arrays.cs

--- Unit 1c/arrays.cs
+++ Unit 1c/arrays.cs
@@ -6,7 +6,19 @@ public class Program
     public void Main()
     {
 
-    
+    	string[] weapons = {"Las gun", "Hell Gun", "Bolter Pistol"};
+
+		string[] weapons = {"Las gun", "Hell Gun", "Bolter Pistol", "Flamer", "Heavy Stubber"};
+		
+		
+		foreach(var weapon in weapons)
+		{
+		
+			Console.WriteLine("Your avalible weapons are " + weapon + ".");
+			
+		}
+		
+		Console.WriteLine(weapons[2]);
     
     }
 
