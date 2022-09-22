2b7993d70a62918863cbe19c1c687865587bdc6c
TheBadesman <Cameron@ottleys.net>
2022-09-21 23:42:35 -0600

    Update forLoop.cs
    Worked on for Loop

--- Unit 1c/forLoop.cs
+++ Unit 1c/forLoop.cs
@@ -6,8 +6,23 @@ public class Program
     public void Main()
     {
 
-    
-    
+    	string[] weapons = {"Bolter Pistol", "Bolter Pistol", "Bolter Pistol", "Bolter Pistol", "Bolter Pistol"};
+
+		
+		for(var i = 3; i < weapons.Length; i++)
+		{
+			
+			weapons[i] = "Heavy Bolter";
+			
+		}
+		
+		foreach(var weapon in weapons)
+		{
+		
+			Console.WriteLine("Your avalible weapons are " + weapon + ".");
+			
+		}
+
     }
 
 }
